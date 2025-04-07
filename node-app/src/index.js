02a57e39466a5a1dc53d546f50abc47589f0a156
AndrewFBel <dogkirillbelyakov@gmail.com>
2025-04-07 21:48:41 +0200

    Try test deployment errors

--- node-app/src/index.js
+++ node-app/src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 80;
 
+throw new Error("Simulating a crash");
+
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
